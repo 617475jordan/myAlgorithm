a92788cf87290a33c6314e313b064e5dff4c7368
617475jordan <617475jordan@sina.com>
2018-06-05 16:12:34 +0800

    Add files via upload

---
+++ FindFiles.h
@@ -0,0 +1,23 @@
+/*
+ *   Find and generate a file list of the folder.
+**/
+
+#ifndef FIND_FILES_H
+#define FIND_FILES_H
+
+#include <vector>
+#include <string>
+
+class FindFiles
+{
+public:
+    FindFiles(){}
+    ~FindFiles(){}
+
+    std::vector<std::string> findFiles( const char *lpPath, const char *secName = ".*" );
+
+private:
+    std::vector<std::string> file_lists;
+};
+
+#endif // FIND_FILES_H
